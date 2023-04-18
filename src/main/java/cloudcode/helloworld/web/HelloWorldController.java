0b380d4573a6f04cabab465779dad95d9d62112f
Stuart Barnett <stuart.barnett@cts.co>
2023-04-18 14:56:05 +0000

    first commit

---
+++ src/main/java/cloudcode/helloworld/web/HelloWorldController.java
@@ -0,0 +1,35 @@
+
+package cloudcode.helloworld.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Defines a controller to handle HTTP requests.
+ */
+@Controller
+public final class HelloWorldController {
+
+@Autowired
+private Environment environment;
+
+    /**
+     * Create an endpoint for the landing page
+     * @return the index view template with a simple message
+     */
+    @GetMapping("/")
+    public String helloWorld(Model model) {
+        String message = "It's running!";
+        String msg2 = environment.getProperty("welcome.msg");
+        String toolOptions = environment.getProperty("java.tool.options");
+
+        System.out.println("MSG from env property: " + msg2);
+        System.out.println("java tool options: " + toolOptions);
+        
+        model.addAttribute("message", message);
+        return "index";
+    }
+}
